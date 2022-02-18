29e1d40bfea458abe6b4ce438026a3a1869d50e4
daniyusra <daniyusra@gmail.com>
2022-02-18 13:48:45 +0900

    recyclerview in firstfragment
    - created the recycler view for the bookmark object
    - put the recyclerview in the firstfragment

--- app/src/main/java/com/cjlcboys/bookmarktracker/FirstFragment.java
+++ app/src/main/java/com/cjlcboys/bookmarktracker/FirstFragment.java
@@ -8,8 +8,15 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.cjlcboys.bookmarktracker.bookmarkrecyclerview.Bookmark;
+import com.cjlcboys.bookmarktracker.bookmarkrecyclerview.BookmarksAdapter;
 import com.cjlcboys.bookmarktracker.databinding.FragmentFirstBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FirstFragment extends Fragment {
 
@@ -22,6 +29,19 @@ public class FirstFragment extends Fragment {
     ) {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
+
+        RecyclerView rvBookmarks = (RecyclerView) binding.rvBookmarks;
+
+        List<Bookmark> bookmarks = new ArrayList<>();
+
+        bookmarks.add(new Bookmark("BRUH","BRUH.COM","BRUHHHH"));
+        bookmarks.add(new Bookmark("BRUH2","BRUH.COM","BRUHHHH"));
+
+        BookmarksAdapter adapter = new BookmarksAdapter(bookmarks);
+        // Attach the adapter to the recyclerview to populate items
+        rvBookmarks.setAdapter(adapter);
+        // Set layout manager to position the items
+        rvBookmarks.setLayoutManager(new LinearLayoutManager(getActivity()));
         return binding.getRoot();
 
     }
---
+++ app/src/main/java/com/cjlcboys/bookmarktracker/bookmarkrecyclerview/BookmarksAdapter.java
@@ -0,0 +1,82 @@
+package com.cjlcboys.bookmarktracker.bookmarkrecyclerview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.cjlcboys.bookmarktracker.R;
+
+import java.util.List;
+
+public class BookmarksAdapter  extends
+        RecyclerView.Adapter<BookmarksAdapter.ViewHolder>{
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // Your holder should contain a member variable
+        // for any view that will be set as you render a row
+        public TextView bookmarkTextView;
+        public TextView bookmarkUrlView;
+        public Button editButton;
+
+        // We also create a constructor that accepts the entire item row
+        // and does the view lookups to find each subview
+        public ViewHolder(View itemView) {
+            // Stores the itemView in a public final member variable that can be used
+            // to access the context from any ViewHolder instance.
+            super(itemView);
+
+            bookmarkTextView= (TextView) itemView.findViewById(R.id.bookmark_title);
+            bookmarkUrlView = (TextView) itemView.findViewById(R.id.bookmark_url);
+            editButton = (Button) itemView.findViewById(R.id.edit_button);
+        }
+    }
+
+    // Store a member variable for the contacts
+    private List<Bookmark> mBookmarks;
+
+    // Pass in the contact array into the constructor
+    public BookmarksAdapter(List<Bookmark> bookmarks) {
+        mBookmarks = bookmarks;
+    }
+
+    @NonNull
+    @Override
+    public BookmarksAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View contactView = inflater.inflate(R.layout.bookmark_item, parent, false);
+
+        // Return a new holder instance
+        ViewHolder viewHolder = new ViewHolder(contactView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BookmarksAdapter.ViewHolder holder, int position) {
+        // Get the data model based on position
+        Bookmark bookmark = mBookmarks.get(position);
+
+        // Set item views based on your views and data model
+        TextView textView1 = holder.bookmarkTextView;
+        textView1.setText(bookmark.getTitle());
+        TextView textView2 = holder.bookmarkUrlView;
+        textView2.setText(bookmark.getUrl());
+        Button button = holder.editButton;
+        button.setText("EDIT");
+        //button.setText(contact.isOnline() ? "Message" : "Offline");
+        //button.setEnabled(contact.isOnline());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mBookmarks.size();
+    }
+}
