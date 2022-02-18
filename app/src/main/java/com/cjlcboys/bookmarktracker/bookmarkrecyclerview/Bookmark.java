ea0ed2b8dc80804eaa2eb7ab72819f1e3d92683a
Samrat Thapa <samratthapa120@gmail.com>
2022-02-18 15:26:16 +0900

    bookmarks

--- app/src/main/java/com/cjlcboys/bookmarktracker/bookmarkrecyclerview/Bookmark.java
+++ app/src/main/java/com/cjlcboys/bookmarktracker/bookmarkrecyclerview/Bookmark.java
@@ -2,7 +2,21 @@ package com.cjlcboys.bookmarktracker.bookmarkrecyclerview;
 
 import java.util.Date;
 import java.util.UUID;
+import org.json.JSONObject;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class Bookmark {
+    private static final String JSON_OBJECT_UUID_HOLDER="JSON_OBJECT_UUID_HOLDER";
+    private static final String JSON_OBJECT_LINK_HOLDER="JSON_OBJECT_LINK_HOLDER";
+    private static final String JSON_OBJECT_DESCRIPTION_HOLDER="JSON_OBJECT_DESCRIPTION_HOLDER";
+    private static final String JSON_OBJECT_TITLE_HOLDER="JSON_OBJECT_TITLE_HOLDER";
+    private static final String JSON_OBJECT_UPLOADTIME_HOLDER="JSON_OBJECT_UPLOADTIME_HOLDER";
+    private static final String JSON_OBJECT_ENDTIME_HOLDER="JSON_OBJECT_ENDTIME_HOLDER";
+
+    private static final SimpleDateFormat simpleDateFormat= new SimpleDateFormat("HH:mm:ss");
+    private static final calendar = Calendar.getInstance();
+
     public String id;
     private String title;
     private String url;
@@ -12,20 +26,41 @@ public class Bookmark {
 //    private static int counter = 0;
 
     //public final int objectId;
+//    private static Date getCurrentDate(){
+//        return simpleDateFormat.format(calendar.getTime());
+//    }
 
-
-    public Bookmark(String title, String url, String desc){
+    public Bookmark(String title, String url, String desc,Date uptime,Date endtime){
         this.id = UUID.randomUUID().toString();
         this.title = title;
         this.url = url;
         this.desc = desc;
+        this.uploadTime = uptime;
+        this.endTime = endtime;
     }
 
-    public Bookmark(String title, String url){
+    public Bookmark(String title, String url, Date uptime,Date endtime){
         this.id = UUID.randomUUID().toString();
         this.title = title;
         this.url = url;
         this.desc = "";
+        this.uploadTime = uptime;
+        this.endTime = endtime;
+    }
+
+    public Bookmark(JSONObject job){
+        if(job.has(JSON_OBJECT_UUID_HOLDER))
+            this.id = job.getString(JSON_OBJECT_UUID_HOLDER);
+        if(job.has(JSON_OBJECT_LINK_HOLDER))
+            this.url = job.getString(JSON_OBJECT_LINK_HOLDER);
+        if(job.has(JSON_OBJECT_TITLE_HOLDER))
+            this.title = job.getString(JSON_OBJECT_TITLE_HOLDER);
+        if(job.has(JSON_OBJECT_DESCRIPTION_HOLDER))
+            this.desc = job.getString(JSON_OBJECT_DESCRIPTION_HOLDER);
+        if(job.has(JSON_OBJECT_UPLOADTIME_HOLDER))
+            this.uploadTime = simpleDateFormat.parse(job.getString(JSON_OBJECT_UPLOADTIME_HOLDER));
+        if(job.has(JSON_OBJECT_ENDTIME_HOLDER))
+            this.endTime = simpleDateFormat.parse(job.getString(JSON_OBJECT_ENDTIME_HOLDER));
     }
 
     //GETTERS
@@ -58,6 +93,17 @@ public class Bookmark {
 
     public void setDesc(String desc){
         this.desc = desc;
+    }
+
+    public JSONObject getJSONObject(){
+        JSONObject job = new JSONObject();
+        job.put(JSON_OBJECT_UUID_HOLDER, this.id);
+        job.put(JSON_OBJECT_LINK_HOLDER, this.url);
+        job.put(JSON_OBJECT_TITLE_HOLDER, this.title);
+        job.put(JSON_OBJECT_DESCRIPTION_HOLDER, this.desc);
+        job.put(JSON_OBJECT_UPLOADTIME_HOLDER, simpleDateFormat.format(this.uploadTime));
+        job.put(JSON_OBJECT_ENDTIME_HOLDER, simpleDateFormat.format(this.endTime));
+        return job;
     }
 
 
